b3be95b6da685b806606272eb9bf633d26782809
Wastard <Ilya.pakylov@gmail.com>
2024-12-09 19:01:00 +0300

    setup project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import path from "path"
+import Unfonts from "unplugin-fonts/vite"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Unfonts({
+      google: {
+        families: ["Open Sans", "Roboto"],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@modules": path.resolve(__dirname, "./src/modules"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+    },
+  },
 })
